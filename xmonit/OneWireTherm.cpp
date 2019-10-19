c6dcd69e697687e2c61a5163542f77fc35d4472e
Paul Cinnamond <pkcinna@xmonit.com>
2019-10-19 19:41:49 -0400

    SOL-5 Add One-Wire DS18B20 temperature sensor support using PI w1-gpio-therm driver

---
+++ xmonit/OneWireTherm.cpp
@@ -0,0 +1,79 @@
+
+#include "OneWireTherm.h"
+
+#include "Poco/Glob.h"
+#include "Poco/StreamCopier.h"
+#include "Poco/Exception.h"
+#include "Poco/NumberParser.h"
+#include "Poco/Path.h"
+#include "Poco/FileStream.h"
+#include "Poco/Util/LayeredConfiguration.h"
+
+#include <fstream>
+
+namespace xmonit {
+
+float OneWireThermSensor::getValueImpl() const {
+
+  //cout << __PRETTY_FUNCTION__ << " name=" << name << ", fileName=" << filePath << ", title=" << title << endl;
+
+  float fTempCelcius(0);
+  std::string line;
+
+  try {
+    stringstream sstr("[one-wire name='");
+    sstr << name << "' path='" << filePath << "'] ERROR:";
+    ifstream fis(filePath, std::ios::in);
+    if ( !std::getline(fis,line) ) {
+      sstr << "Failed reading first line";
+      throw Poco::Exception(sstr.str());
+    } else if ( false ) {
+      sstr << "Expected \".* YES\" on first line";
+      throw Poco::Exception(sstr.str());
+    } else if ( !std::getline(fis,line) ) {
+      sstr << "Failed reading second line";
+      throw Poco::Exception(sstr.str());
+    } else if ( false ) {
+      sstr << "Expected \".* t=[0-9]{5}$\" on second line";
+      throw Poco::Exception(sstr.str());
+    } else {
+      int iTemperatureData = Poco::NumberParser::parse(line.substr(line.size()-5));
+      fTempCelcius = iTemperatureData / 1000.0;
+    }
+  } catch ( const Poco::Exception& ex ) {
+    cerr << __PRETTY_FUNCTION__ << " Exception: " << ex.displayText() << endl;
+    fTempCelcius = NAN;
+  } catch ( ... ) {
+    cerr << __PRETTY_FUNCTION__ << " Exception occured" << endl;
+    fTempCelcius = NAN;
+  }
+
+  if ( fTempCelcius != fLastResult ) {
+    if ( pListener ) {
+      pListener->changed(this,fTempCelcius,fLastResult);
+    }
+    fLastResult = fTempCelcius;
+  }
+  return fTempCelcius;
+}
+
+void OneWireThermSensor::createMatching(vector<unique_ptr<OneWireThermSensor>>& sensors, Poco::Util::LayeredConfiguration& conf) {
+      
+  std::string filter = conf.getString("oneWire[@filter]", "28-*");
+  std::string path = conf.getString("oneWire[@path]", "/sys/bus/w1/devices");
+  std::string search = path + "/" + filter + "/w1_slave";
+
+  std::set<string> fileNames;
+  Poco::Glob::glob(search.c_str(),fileNames);
+  for( string fileName : fileNames ) {
+      string name = Poco::Path(fileName).parent().makeFile().getFileName();
+      string xpath = "oneWire.titles.title[@id='";
+      xpath += name;
+      xpath += "'][@value]";
+      string title = conf.getString( xpath, name);
+      cout << __PRETTY_FUNCTION__ << " name=" << name << ", fileName=" << fileName << ", title=" << title << ", xpath=" << xpath << endl;
+      sensors.push_back( make_unique<OneWireThermSensor>(name,fileName,title));
+  }
+}
+
+}
---
+++ xmonit/OneWireTherm.h
@@ -0,0 +1,41 @@
+#ifndef XMONIT_ONEWIRETHERM_H
+#define XMONIT_ONEWIRETHERM_H
+
+// sensor backed by a "one-wire" temp sensors (DS18B20) 
+
+#include "xmonit.h"
+#include "../automation/sensor/Sensor.h"
+
+#include "Poco/Util/LayeredConfiguration.h"
+
+#include <memory>
+
+
+namespace xmonit {
+
+  class OneWireThermSensor : public automation::Sensor {
+  public:
+    RTTI_GET_TYPE_IMPL(xmonit,OneWireThermSensor);
+
+    std::unique_ptr<automation::SensorListener> pListener;
+    string title, filePath;
+
+    OneWireThermSensor(const string &name, const string& filePath, const string &title ) : 
+      automation::Sensor(name,1),
+      title(title),
+      filePath(filePath) {
+    }
+    
+    virtual float getValueImpl() const override;
+    
+    virtual string getTitle() const override {
+      return title;
+    }
+
+    static void createMatching(vector<unique_ptr<OneWireThermSensor>>& sensors, Poco::Util::LayeredConfiguration& conf);
+     
+    private:
+    mutable float fLastResult{0};
+ };
+}
+#endif 
